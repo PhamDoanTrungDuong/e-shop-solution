596077d8e8426190e74d243a5656bba14189c20b
Dương Phạm <pdtduong03@gmail.com>
2021-08-17 17:13:08 +0700

    DBset

---
+++ eShopSolution.Data/Entities/Category.cs
@@ -0,0 +1,16 @@
+﻿using eShopSolution.Data.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public int SortOder { get; set; }
+        public bool isShowOnHome { get; set; }
+        public int? Parentid { get; set; }
+        public Status Status { get; set; }
+    }
+}
