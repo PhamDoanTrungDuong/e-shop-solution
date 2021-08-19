4df84e77e12cc112526099e61a35f6a4278a15a2
Dương Phạm <pdtduong03@gmail.com>
2021-08-19 11:29:56 +0700

    seeding migration

--- eShopSolution.Data/Entities/Product.cs
+++ eShopSolution.Data/Entities/Product.cs
@@ -14,8 +14,8 @@ namespace eShopSolution.Data.Entities
         public decimal OriginalPrice { get; set; }
         public int Stock { get; set; }
         public int ViewCount { get; set; }
-        public DateTime DataCreated { get; set; }  
-        public string SeoAlias { get; set; }
+        public DateTime DateCreated { get; set; }  
+      
         public List<ProductInCategory> ProductInCategories { get; set; }
 
         public List<OrderDetail> OrderDetails { get; set; }
