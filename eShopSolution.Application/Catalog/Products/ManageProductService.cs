12a1d7dab012f6c7e43918f8f36d3ad5bd43d68c
Dương Phạm <pdtduong03@gmail.com>
2021-08-22 10:10:34 +0700

    backendAPI, add swagger

--- eShopSolution.Application/Catalog/Products/ManageProductService.cs
+++ eShopSolution.Application/Catalog/Products/ManageProductService.cs
@@ -4,7 +4,6 @@ using eShopSolution.Data.EF;
 using eShopSolution.Data.Entities;
 using eShopSolution.Utilities.Exceptions;
 using eShopSolution.ViewModels.Catalog.Products;
-using eShopSolution.ViewModels.Catalog.Products.Manage;
 using eShopSolution.ViewModels.Common;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
@@ -102,7 +101,7 @@ namespace eShopSolution.Application.Catalog.Products
         }
 
 
-        public async Task<PagedResult<ProductViewModel>> GetAllPaging(GetProductPagingRequest request)
+        public async Task<PagedResult<ProductViewModel>> GetAllPaging(GetManageProductPagingRequest request)
         {
             //1.Select Join
             var query = from p in _context.Products
