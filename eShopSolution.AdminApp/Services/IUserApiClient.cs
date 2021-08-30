a96154797c514736da8b6934831e5175594f6e36
Dương Phạm <pdtduong03@gmail.com>
2021-08-30 12:06:57 +0700

    update users

--- eShopSolution.AdminApp/Services/IUserApiClient.cs
+++ eShopSolution.AdminApp/Services/IUserApiClient.cs
@@ -1,18 +1,20 @@
 ﻿using eShopSolution.ViewModels.Common;
 using eShopSolution.ViewModels.System.Users;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace eShopSolution.AdminApp.Services
 {
     public interface IUserApiClient
     {
-        Task<string> Authenticate(LoginRequest request);
+        Task<ApiResult<string>> Authenticate(LoginRequest request);
 
-        Task<PagedResult<UserViewModel>> GetUserPagings(GetUserPagingRequest request);
+        Task<ApiResult<PagedResult<UserViewModel>>> GetUserPagings(GetUserPagingRequest request);
 
-        Task<bool> RegisterUser(RegisterRequest register);
+        Task<ApiResult<bool>> RegisterUser(RegisterRequest register);
+
+        Task<ApiResult<bool>> UpdateUser(Guid id, UserUpdateRequest register);
+
+        Task<ApiResult<UserViewModel>> GetById(Guid id);
     }
 }
