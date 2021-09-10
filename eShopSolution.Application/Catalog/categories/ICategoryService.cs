603cf47e351d4a8072de09bc0a0d07f844f90747
Dương Phạm <pdtduong03@gmail.com>
2021-09-10 13:12:10 +0700

    category product

--- eShopSolution.Application/Catalog/categories/ICategoryService.cs
+++ eShopSolution.Application/Catalog/categories/ICategoryService.cs
@@ -7,5 +7,7 @@ namespace eShopSolution.Application.Catalog.categories
     public interface ICategoryService
     {
         Task<List<CategoryVm>> GetAll(string LanguageId);
+
+        Task<CategoryVm> GetById(string languageId, int id);
     }
 }
